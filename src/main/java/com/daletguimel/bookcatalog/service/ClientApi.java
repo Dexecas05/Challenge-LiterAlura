7f2308f038459a64287cb5e7c4c3ee008f8119a9
Exequiel Castillo <execas.dec@gmail.com>
2024-11-28 11:19:08 -0300

    añado la clase ClientApi para la solicitud http

---
+++ src/main/java/com/daletguimel/bookcatalog/service/ClientApi.java
@@ -0,0 +1,28 @@
+package com.daletguimel.bookcatalog.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ClientApi {
+    private final HttpClient client;
+
+    public ClientApi() {
+        this.client = HttpClient.newHttpClient();
+    }
+
+    public String getBooks(String query) throws IOException, InterruptedException{
+        String url = "https://gutendex.com/books" + query;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() == 200) {
+            return response.body();
+        } else {
+            throw new RuntimeException("Error en la búsqueda: " + response.statusCode());
+        }
+    }
+}
